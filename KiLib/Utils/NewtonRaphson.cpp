09e3d6c5806f08eef62b11901244c479fb0ec005
Denis Cohen-Corticchiato <denis.cohen@protonmail.com>
2020-12-14 20:44:04 -0700

    Print iteration progress in debug mode for NewtonRaphson utility

--- KiLib/Utils/NewtonRaphson.cpp
+++ KiLib/Utils/NewtonRaphson.cpp
@@ -12,6 +12,7 @@ double NewtonRaphson::NewtonRaphsonAnalytic(
    auto dx   = std::numeric_limits<double>::max();
    auto x1   = x_o;
    auto x2   = x_o;
+   spdlog::debug("Iter        x_n    x_{n+1}           dx");
    do {
       iter++;
       x1        = x2;
@@ -22,7 +23,7 @@ double NewtonRaphson::NewtonRaphsonAnalytic(
       }
       x2 = x1 - f(x1) / dfx1;
       dx = std::abs(x2 - x1);
-      // spdlog::info("{} {} {} {}", iter, x1, x2, dx);
+      spdlog::debug("{:4d} {:10.6f} {:10.6f} {:12.6e}", iter, x1, x2, dx);
    } while ((iter < maxIter) && (dx > tol));
    if (iter >= maxIter) {
       spdlog::error("Newton Raphson Analytic: number of iterations exceeded");
