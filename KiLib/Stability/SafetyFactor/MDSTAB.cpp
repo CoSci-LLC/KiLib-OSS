aa8faecd373bfdbe88cf2443b107d0c5fe3cbfac
Haydn Jones <haydnjonest@gmail.com>
2021-01-02 17:03:56 -0700

    Removed implicit casting

--- KiLib/Stability/SafetyFactor/MDSTAB.cpp
+++ KiLib/Stability/SafetyFactor/MDSTAB.cpp
@@ -25,23 +25,27 @@ double MDSTab::ComputeSF(
    const double K0 = 1.0 - sphi;
 
    // Block cross-slope boundary force (Milledge Equation 7)
-   const double Frl = 0.5 * K0 * (gamma_s - gamma_w * pow(m, 2)) * l * pow(z, 2) * ctheta * tphi + Crl * l * z * ctheta;
+   const double Frl =
+      0.5 * K0 * (gamma_s - gamma_w * pow(m, 2.0)) * l * pow(z, 2.0) * ctheta * tphi + Crl * l * z * ctheta;
 
    // Rankine solution for cohesive soils
    const double K = std::max(
-      0.0, 4.0 * pow(ctheta, 2) * (pow(ctheta, 2) - pow(cphi, 2)) + (4 * pow(Crl / (gamma_s * z), 2) * pow(cphi, 2)) +
-              (8 * (Crl / (gamma_s * z)) * pow(ctheta, 2) * sphi * cphi));
+      0.0, 4.0 * pow(ctheta, 2.0) * (pow(ctheta, 2.0) - pow(cphi, 2.0)) +
+              (4.0 * pow(Crl / (gamma_s * z), 2.0) * pow(cphi, 2.0)) +
+              (8.0 * (Crl / (gamma_s * z)) * pow(ctheta, 2.0) * sphi * cphi));
 
    // Passive (p) and active (a) earth pressure coefficients for Rankine solution for cohesive soils (Milledge Equation
    // 8)
-   const double Kp = (1 / pow(cphi, 2)) * (2 * pow(ctheta, 2) + 2 * (Crl / (gamma_s * z)) * cphi * sphi + sqrt(K)) - 1;
-   const double Ka = (1 / pow(cphi, 2)) * (2 * pow(ctheta, 2) + 2 * (Crl / (gamma_s * z)) * cphi * sphi - sqrt(K)) - 1;
+   const double Kp =
+      (1.0 / pow(cphi, 2.0)) * (2.0 * pow(ctheta, 2.0) + 2.0 * (Crl / (gamma_s * z)) * cphi * sphi + sqrt(K)) - 1.0;
+   const double Ka =
+      (1.0 / pow(cphi, 2.0)) * (2.0 * pow(ctheta, 2.0) + 2.0 * (Crl / (gamma_s * z)) * cphi * sphi - sqrt(K)) - 1.0;
 
    // Slope-parallel passive force on the upslope margin (Milledge Equation 12 modified)
-   const double Fdu = 0.5 * Ka * pow(z, 2) * (gamma_s - gamma_w * pow(m, 2)) * w * cos(delta - theta);
+   const double Fdu = 0.5 * Ka * pow(z, 2.0) * (gamma_s - gamma_w * pow(m, 2.0)) * w * cos(delta - theta);
 
    // Slope-normal passive force on the upslope margin (Milledge Equation 13 modified)
-   const double Fnu = 0.5 * Ka * pow(z, 2) * (gamma_s - gamma_w * pow(m, 2)) * w * sin(delta - theta);
+   const double Fnu = 0.5 * Ka * pow(z, 2.0) * (gamma_s - gamma_w * pow(m, 2.0)) * w * sin(delta - theta);
 
    // Slope-parallel passive force on the downslope margin (Milledge Equation 12)
    // const double Frd = 0.5 * Kp * pow(z, 2) * (gamma_s - gamma_w * pow(m, 2)) * w * cos(delta - theta);
@@ -50,10 +54,10 @@ double MDSTab::ComputeSF(
    const double Frd = 0.0;
 
    // Slope-normal passive force on the downslope margin (Milledge Equation 13)
-   const double Fnd = 0.5 * Kp * pow(z, 2) * (gamma_s - gamma_w * pow(m, 2)) * w * sin(delta - theta);
+   const double Fnd = 0.5 * Kp * pow(z, 2.0) * (gamma_s - gamma_w * pow(m, 2.0)) * w * sin(delta - theta);
 
    // Weight of block (see Milledge Equation 14)
-   const double Fnc = (gamma_s - gamma_w * m) * z * pow(ctheta, 2) * w * l;
+   const double Fnc = (gamma_s - gamma_w * m) * z * pow(ctheta, 2.0) * w * l;
 
    // Normal force on basal surface (Milledge Equation 14)
    const double Fnt = Fnc + Fnu - Fnd;
@@ -62,7 +66,7 @@ double MDSTab::ComputeSF(
    const double Frb = Crb * w * l + Fnt * tphi;
 
    // Factor of Safety (Milledge Equation 16)
-   const double FS = (Frb + 2 * Frl + Frd - Fdu) / Fdc;
+   const double FS = (Frb + 2.0 * Frl + Frd - Fdu) / Fdc;
 
    return FS;
 }
