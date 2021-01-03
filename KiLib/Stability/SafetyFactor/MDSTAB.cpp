19d643675ecd3983f0cfd1b831a110b18c864bb3
Haydn Jones <haydnjonest@gmail.com>
2021-01-02 17:02:49 -0700

    Precomputing trig functions

--- KiLib/Stability/SafetyFactor/MDSTAB.cpp
+++ KiLib/Stability/SafetyFactor/MDSTAB.cpp
@@ -11,55 +11,58 @@ double MDSTab::ComputeSF(
    double phi, double m, double z, double Crl, double Crb, double theta, double delta, double gamma_s, double w,
    double l) const
 {
+   const double stheta = std::sin(theta);
+   const double ctheta = std::cos(theta);
+
+   const double sphi = std::sin(phi);
+   const double cphi = std::cos(phi);
+   const double tphi = std::tan(phi);
+
    // Central block driving force (Milledge Equation 3)
-   double Fdc = gamma_s * z * sin(theta) * cos(theta) * w * l;
+   const double Fdc = gamma_s * z * stheta * ctheta * w * l;
 
    // At-rest earth pressure coefficient (Milledge Equation 5)
-   double K0 = 1.0 - sin(phi);
+   const double K0 = 1.0 - sphi;
 
    // Block cross-slope boundary force (Milledge Equation 7)
-   double Frl =
-      0.5 * K0 * (gamma_s - gamma_w * pow(m, 2)) * l * pow(z, 2) * cos(theta) * tan(phi) + Crl * l * z * cos(theta);
+   const double Frl = 0.5 * K0 * (gamma_s - gamma_w * pow(m, 2)) * l * pow(z, 2) * ctheta * tphi + Crl * l * z * ctheta;
 
    // Rankine solution for cohesive soils
-   double K = std::max(
-      0.0, 4.0 * pow(cos(theta), 2) * (pow(cos(theta), 2) - pow(cos(phi), 2)) +
-              (4 * pow(Crl / (gamma_s * z), 2) * pow(cos(phi), 2)) +
-              (8 * (Crl / (gamma_s * z)) * pow(cos(theta), 2) * sin(phi) * cos(phi)));
+   const double K = std::max(
+      0.0, 4.0 * pow(ctheta, 2) * (pow(ctheta, 2) - pow(cphi, 2)) + (4 * pow(Crl / (gamma_s * z), 2) * pow(cphi, 2)) +
+              (8 * (Crl / (gamma_s * z)) * pow(ctheta, 2) * sphi * cphi));
 
    // Passive (p) and active (a) earth pressure coefficients for Rankine solution for cohesive soils (Milledge Equation
    // 8)
-   double Kp =
-      (1 / pow(cos(phi), 2)) * (2 * pow(cos(theta), 2) + 2 * (Crl / (gamma_s * z)) * cos(phi) * sin(phi) + sqrt(K)) - 1;
-   double Ka =
-      (1 / pow(cos(phi), 2)) * (2 * pow(cos(theta), 2) + 2 * (Crl / (gamma_s * z)) * cos(phi) * sin(phi) - sqrt(K)) - 1;
+   const double Kp = (1 / pow(cphi, 2)) * (2 * pow(ctheta, 2) + 2 * (Crl / (gamma_s * z)) * cphi * sphi + sqrt(K)) - 1;
+   const double Ka = (1 / pow(cphi, 2)) * (2 * pow(ctheta, 2) + 2 * (Crl / (gamma_s * z)) * cphi * sphi - sqrt(K)) - 1;
 
    // Slope-parallel passive force on the upslope margin (Milledge Equation 12 modified)
-   double Fdu = 0.5 * Ka * pow(z, 2) * (gamma_s - gamma_w * pow(m, 2)) * w * cos(delta - theta);
+   const double Fdu = 0.5 * Ka * pow(z, 2) * (gamma_s - gamma_w * pow(m, 2)) * w * cos(delta - theta);
 
    // Slope-normal passive force on the upslope margin (Milledge Equation 13 modified)
-   double Fnu = 0.5 * Ka * pow(z, 2) * (gamma_s - gamma_w * pow(m, 2)) * w * sin(delta - theta);
+   const double Fnu = 0.5 * Ka * pow(z, 2) * (gamma_s - gamma_w * pow(m, 2)) * w * sin(delta - theta);
 
    // Slope-parallel passive force on the downslope margin (Milledge Equation 12)
-   // double Frd = 0.5 * Kp * pow(z, 2) * (gamma_s - gamma_w * pow(m, 2)) * w * cos(delta - theta);
+   // const double Frd = 0.5 * Kp * pow(z, 2) * (gamma_s - gamma_w * pow(m, 2)) * w * cos(delta - theta);
 
    // THIS IS IN THE MATLAB CODE. MAY NEED AN OPTION HERE TO NOT SET IT TO ZERO AND USE EQUATION ABOVE. TBD
-   double Frd = 0.0;
+   const double Frd = 0.0;
 
    // Slope-normal passive force on the downslope margin (Milledge Equation 13)
-   double Fnd = 0.5 * Kp * pow(z, 2) * (gamma_s - gamma_w * pow(m, 2)) * w * sin(delta - theta);
+   const double Fnd = 0.5 * Kp * pow(z, 2) * (gamma_s - gamma_w * pow(m, 2)) * w * sin(delta - theta);
 
    // Weight of block (see Milledge Equation 14)
-   double Fnc = (gamma_s - gamma_w * m) * z * pow(cos(theta), 2) * w * l;
+   const double Fnc = (gamma_s - gamma_w * m) * z * pow(ctheta, 2) * w * l;
 
    // Normal force on basal surface (Milledge Equation 14)
-   double Fnt = Fnc + Fnu - Fnd;
+   const double Fnt = Fnc + Fnu - Fnd;
 
    // Basal resistance force (Milledge Equation 15)
-   double Frb = Crb * w * l + Fnt * tan(phi);
+   const double Frb = Crb * w * l + Fnt * tphi;
 
    // Factor of Safety (Milledge Equation 16)
-   double FS = (Frb + 2 * Frl + Frd - Fdu) / Fdc;
+   const double FS = (Frb + 2 * Frl + Frd - Fdu) / Fdc;
 
    return FS;
 }
