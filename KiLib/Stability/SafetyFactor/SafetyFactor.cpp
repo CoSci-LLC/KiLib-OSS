d9e0fb35831598d90b14374525b6b36e0f23bdc9
Haydn Jones <haydnjonest@gmail.com>
2021-02-04 09:17:43 -0700

    Added cpp back

---
+++ KiLib/Stability/SafetyFactor/SafetyFactor.cpp
@@ -0,0 +1,9 @@
+#include <KiLib/Exceptions/NotImplemented.hpp>
+#include <KiLib/Stability/SafetyFactor/SafetyFactor.hpp>
+
+using namespace KiLib::Stability::SafetyFactor;
+
+double SafetyFactor::ComputeSF()
+{
+   throw NotImplementedException("Method not implemented");
+}
