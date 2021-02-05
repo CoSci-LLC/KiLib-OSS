32d5a45170054b66ab2076cd892917407f36fdd1
Owen Parkins <oparkins@gmail.com>
2021-02-05 16:47:01 -0500

    [doc] Added copyright to beginning of each file
    Close #38

--- KiLib/Utils/StatisticalOutput.hpp
+++ KiLib/Utils/StatisticalOutput.hpp
@@ -1,4 +1,24 @@
 /**
+ *  Copyright (c) 2020-2021 CoSci LLC, USA <software@cosci-llc.com>
+ *
+ *  This file is part of KiLib-OSS.
+ *
+ *  KiLib-OSS is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  KiLib-OSS is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with KiLib-OSS.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+
+/**
  * @file StatisticalOutput.hpp
  *
  * @brief Provides a on the fly calculation of mean and std. dev.
