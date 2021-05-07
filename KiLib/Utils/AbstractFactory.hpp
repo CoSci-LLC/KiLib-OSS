7a8460a72bed6eeb35f8e5e92ff0b660c1b7307c
Haydn Jones <haydnjonest@gmail.com>
2021-05-06 18:28:24 -0600

    whitespace insensitive absfac

--- KiLib/Utils/AbstractFactory.hpp
+++ KiLib/Utils/AbstractFactory.hpp
@@ -6,7 +6,7 @@
 #include <map>
 #include <spdlog/spdlog.h>
 #include <typeinfo>
-
+#include <algorithm>
 
 template <typename Enum, class Base> class AbsFac
 {
@@ -26,11 +26,14 @@ public:
    {
       try
       {
-         return this->map.at(Enum::_from_string_nocase(val.c_str()))();
+         // Erase spaces because of tree species readability
+         std::string clean = this->removeSpaces(val);
+         return this->map.at(Enum::_from_string_nocase(clean.c_str()))();
       }
       catch (std::exception &e)
       {
          spdlog::error("Unknown {} '{}'! Available options: {}", Enum::_name(), val, fmt::join(Enum::_names(), ", "));
+         spdlog::error("Note: These values are case insensitive and ignore spaces!");
          exit(EXIT_FAILURE);
       }
    }
@@ -44,12 +47,22 @@ public:
    {
       try
       {
-         return Enum::_from_string_nocase(val.c_str());
+         // Erase spaces because of tree species readability
+         std::string clean = this->removeSpaces(val);
+         return Enum::_from_string_nocase(clean.c_str());
       }
       catch (std::exception &e)
       {
          spdlog::error("Unknown {} '{}'! Available options: {}", Enum::_name(), val, fmt::join(Enum::_names(), ", "));
+         spdlog::error("Note: These values are case insensitive and ignore spaces!");
          exit(EXIT_FAILURE);
       }
+   }
+
+private:
+   std::string removeSpaces(std::string input) const
+   {
+     input.erase(std::remove(input.begin(),input.end(),' '),input.end());
+     return input;
    }
 };
