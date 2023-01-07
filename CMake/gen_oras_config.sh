205684d5affaf8cc7caaf882e7e49fcb53f5e992
Owen Parkins <oparkins@gmail.com>
2023-01-07 02:28:04 -0500

    [WIP] Adding push capabilities

---
+++ CMake/gen_oras_config.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+if [[ -f /etc/os-release ]]; then
+	OS=$(awk  -F '=' '/^NAME/{print $2}' /etc/os-release)
+	RELEASE=$(awk  -F '=' '/^VERSION_ID/{print $2}' /etc/os-release)
+else
+	OS="UNKNOWN"
+	RELEASE="UNKNOWN"
+fi
+
+ARCH="$(uname -m)"
+LIBRARY="KiLib-OSS"
+GIT_HASH=$(git log -1 --format='%h')
+
+cat << EOF 
+{
+    "os": ${OS},
+    "release": "${RELEASE}",
+    "mediaType": "appication/octet-stream",
+    "architecture": "${ARCH}",
+    "library": "${LIBRARY}",
+    "maintainer": "dev@cosci-llc.com",
+    "git-hash": "${GIT_HASH:-UNKNOWN}"
+}
+EOF
+
