d1ed203a12348a2f0702d0b2c6da69514538ac26
Denis Cohen-Corticchiato <denis.cohen@gmail.com>
2024-04-27 19:32:02 +0200

    Remove comments to function

--- KiLib/Raster/Raster.cpp
+++ KiLib/Raster/Raster.cpp
@@ -399,15 +399,15 @@ namespace KiLib
             {
                continue;
             }
-            spdlog::info("  GetCoordMinDist inPos.x = {}, ci = {}", inPos.x, static_cast<double>(ci));
-            spdlog::info("  GetCoordMinDist inPos.y = {}, ri = {}", inPos.y, static_cast<double>(ri));
+            //spdlog::info("  GetCoordMinDist inPos.x = {}, ci = {}", inPos.x, static_cast<double>(ci));
+            //spdlog::info("  GetCoordMinDist inPos.y = {}, ri = {}", inPos.y, static_cast<double>(ri));
             // This can probably be done faster, handles the corners being out of the radius
             //const double dr   = std::abs((double)(r - ri)) * cellsize;
             //const double dc   = std::abs((double)(c - ci)) * cellsize;
             const double dr   = inPos.y - (yllcorner + static_cast<double>(ri) * cellsize + cellsize / 2.0);
             const double dc   = inPos.x - (xllcorner + static_cast<double>(ci) * cellsize + cellsize / 2.0);
             const double dist = sqrt(dr * dr + dc * dc);
-            spdlog::info("  GetCoordMinDist dr = {}; dc = {}; dist = {}", dr, dc, dist);
+            //spdlog::info("  GetCoordMinDist dr = {}; dc = {}; dist = {}", dr, dc, dist);
             if (dist > radius)
             {
                SPDLOG_DEBUG("SKIPPING\n");
