7cd3d8aa444703fd87335c6a21059d62c3c3ca52
Denis Cohen-Corticchiato <denis.cohen@gmail.com>
2025-07-20 15:42:38 +0200

    Add DEBUG statements

--- KiLib/Hydrology/Bonetti2021.cpp
+++ KiLib/Hydrology/Bonetti2021.cpp
@@ -22,6 +22,7 @@
 #include <KiLib/Hydrology/Hydrology.hpp>
 #include <algorithm>
 #include <cmath>
+#include <spdlog/spdlog.h>
 
 using namespace KiLib::Hydrology;
 
@@ -35,6 +36,7 @@ double Bonetti2021::ComputeInfiltration(
 {
    const auto mean  = std::log( std::pow(ks_mean,2) / (std::sqrt(std::pow(ks_mean,2) + std::pow(ks_std,2))));
    const auto sigma = std::sqrt( std::log( 1 + std::pow((ks_std/mean),2)));
+   SPDLOG_DEBUG("ks mean = {} sigma = {}", mean, sigma);
 
 
    const auto &x = rainfall;
@@ -42,6 +44,7 @@ double Bonetti2021::ComputeInfiltration(
    const auto term1 = x / 2.0;
    const auto term2 = x / 2.0 * std::erf( (mean + std::log(1.0 / 3.0 / x)) / (sqrt2 * sigma) );
    const auto term3 = (1.0 / 6.0) * std::exp(mean + std::pow(sigma,2) / 2.0) * std::erfc( (mean + std::pow(sigma,2) - std::log(3.0 * x)) / (sigma * sqrt2));
+   SPDLOG_DEBUG("term1 = {} term2 = {} term3 = {}", term1, term2, term3);
    return term1 + term2 + term3;
 }
 // clang-format on
