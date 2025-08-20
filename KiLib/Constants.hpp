51015bbcc88f2b56ff5f65f743fcd20c2882ebdb
Denis Cohen-Corticchiato <denis.cohen@gmail.com>
2025-08-20 08:33:35 +0200

    Minor clean up

--- KiLib/Constants.hpp
+++ KiLib/Constants.hpp
@@ -7,7 +7,7 @@ namespace KiLib
       inline constexpr double GRAVITY{9.81};                        // [m/s^2] Gravitational acceleration
       inline constexpr double WATER_DENSITY{999.9};                 // [m^3/kg] at 0 C
       inline constexpr double WATER_COMPRESSIBILITY_0C{5.09e-10};   // [1/Pa] at 0 C
-      inline constexpr double WATER_COMPRESSIBILITY_5C{4.940e-10};  // [1/Pa] at 0 C
+      inline constexpr double WATER_COMPRESSIBILITY_5C{4.94e-10};   // [1/Pa] at 0 C
       inline constexpr double WATER_COMPRESSIBILITY_10C{4.78e-10};  // [1/Pa] at 10 C
       inline constexpr double WATER_COMPRESSIBILITY_15C{4.69e-10};  // [1/Pa] at 15 C
       inline constexpr double WATER_COMPRESSIBILITY_20C{4.59e-10};  // [1/Pa] at 20 C
